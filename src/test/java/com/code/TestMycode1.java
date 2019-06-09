c24e97bbd3bd4d71fd6eb3e08613ed38e6d51c16
viharika <v.viharika999@gmail.com>
2019-06-09 07:20:43 +0530

    commit

---
+++ src/test/java/com/code/TestMycode1.java
@@ -0,0 +1,21 @@
+package com.code;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+public class TestMycode1 {
+	
+	
+	@Test
+	public void test() {
+		
+		
+		
+		assertEquals("Build path successfully",Mycode1.fun());
+		
+	}
+
+	
+}
